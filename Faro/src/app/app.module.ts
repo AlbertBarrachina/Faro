ff8bfdfe6588f29fc784003a27ac90b3bf95bacc
Rafael.F <rafa2597@hotmail.com>
2024-02-29 19:21:58 +0100

    base

--- Faro/src/app/app.module.ts
+++ Faro/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    ContactoComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
